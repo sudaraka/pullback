9d077593d995acdc1aa884cad394072eeb804f7d
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-02-21 15:18:07 +0530

    Basic buildable command line script

---
+++ src/cli.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+/**
+ * src/cli.js: executable script for `pullback` CLI utility
+ *
+ * Copyright 2016 Sudaraka Wijesinghe <sudaraka@sudaraka.org>
+ *
+ * This program comes with ABSOLUTELY NO WARRANTY
+ * This is free software, and you are welcome to redistribute it and/or modify
+ * it under the terms of the BSD 2-clause License. See the LICENSE file for more
+ * details.
+ */
+
+import main from './main'
+
+main(process.argv.slice(2))
